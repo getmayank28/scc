1229313a98802c2a7df7c72f26e7e427bbadf780
Mayank sonkar <mayanksonkar@Mayanks-MacBook-Pro.local>
2025-11-23 19:15:08 +0530

    chore: update the script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { FormProvider } from "../contexts";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         {/* Google Tag (gtag.js) */}
+         <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0LE6FWF29D`} 
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-0LE6FWF29D');
+          `}
+        </Script>
         <FormProvider>
-          {/* <Navigation /> */}
           {children}
         </FormProvider>
       </body>
