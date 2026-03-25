25b57a0574fbd85eb1d4756e4bf0842790ab55b1
Mayank sonkar <mayanksonkar@Mayanks-MacBook-Pro.local>
2026-03-25 23:27:07 +0530

    chore: move event one level up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 "use client";
 import Landing from "@/components/Landing";
+import { trackEvent } from "@/lib/analytics/track";
+import { EventName } from "@/lib/analytics/types";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  
+  useEffect(() => {
+    trackEvent(EventName.LANDING_PAGE, {
+      path: window.location.pathname,
+      referrer: document.referrer,
+    });
+  }, []);
+
+
   return (
     <div className="bg-[#101010] w-full">
       <Landing/>
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -16,20 +16,11 @@ import Waitlist from "./components/Waitlist";
 import WeDonNotPromote from "./components/WeDonNotPromote";
 import { useFeatureFlag } from "@/contexts/FeatureContext";
 import { FeatureFlagsConfig } from "@/lib/constants/featureFlags";
-import { useEffect } from "react";
-import { trackEvent } from "@/lib/analytics/track";
-import { EventName } from "@/lib/analytics/types";
 import FiSenseIntroVideo from "./components/FiSenseIntroVideo";
 
 const Landing = () => {
   const isWaitlistEnabled = useFeatureFlag(FeatureFlagsConfig.WAITLIST);
 
-  useEffect(() => {
-    trackEvent(EventName.LANDING_PAGE, {
-      path: window.location.pathname,
-      referrer: document.referrer,
-    });
-  }, []);
 
   return (
     <div>
