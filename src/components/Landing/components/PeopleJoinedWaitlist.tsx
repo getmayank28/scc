609d852d42d2a28d4556d303bf219da84e408054
Mayank sonkar <mayanksonkar@Mayanks-MacBook-Pro.local>
2026-04-05 21:29:28 +0530

    chore: update waitlist text

--- src/components/Landing/components/PeopleJoinedWaitlist.tsx
+++ src/components/Landing/components/PeopleJoinedWaitlist.tsx
@@ -5,7 +5,7 @@ const PeopleJoinedWaitlist  = () => {
     return (
         <div className="flex items-center max-md:justify-center max-md:pb-10 gap-1">
             <Image width={105} height={40} src="/images/waitlisted-people.png" alt="people"/>
-            <Typography variant="caption" className="text-sm font-bold">Trusted by 250+ early adopters</Typography>
+            <Typography variant="caption" className="text-sm font-bold">Trusted by 400+ early adopters</Typography>
         </div>
 
     )
